a651ae3b81a92f0964a562d966866d6ec25b1cf9
William Lee <wkl@explainedbycats.com>
2022-08-30 19:56:50 -0400

    Use higher-quality logo, add favicons

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,13 @@ const Image = require("@11ty/eleventy-img");
 const markit = require("markdown-it");
 const path = require('node:path');
 
-async function imageShortcode(src, alt, sizes) {
+async function imageShortcode(src, alt, sizes, widths) {
+    if (widths === undefined) {
+        widths = [200, 300, 400, 640, 1600];
+    }
     src = __dirname + "/src/" + src;
     let meta = await Image(src, {
-        widths: [200, 300, 400, 640, 1600],
+        widths: widths,
         formats: ["webp", "png"],
         outputDir: "./_site/images",
         urlPath: "/images/"
@@ -24,6 +27,8 @@ async function imageShortcode(src, alt, sizes) {
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("CNAME");
+    eleventyConfig.addPassthroughCopy({ "flat_assets": "/" });
+
     eleventyConfig.ignores.add("**.md");
     eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
     eleventyConfig.addPairedShortcode("markdown", (content) => {
